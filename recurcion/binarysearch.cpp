f73f2bc0244c773256125bcf2c116ec4813c236a
ruturaj2829 <ruturajsonkamble-ecs@atharvacoe.ac.in>
2023-08-22 09:17:15 +0530

    raj

--- recurcion/binarysearch.cpp
+++ recurcion/binarysearch.cpp
@@ -2,14 +2,27 @@
 #include <cmath>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{    
+int binarysearch(int arr[], int s, int e, int key) {
+    if (s <= e) {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (arr[mid] < key) {
+            return binarysearch(arr, mid + 1, e, key);
+        } else {
+            return binarysearch(arr, s, mid - 1, key);
+        }
+    }
+    return -1;
+}
 
-    int a = 2;
-    int b = 3;
-    int n = 2;
-    int ans  = pow(a,n)+ pow(b,n) ;
-    cout<< ans;
+int main(int argc, char const *argv[]) {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int x = 2;
+    int n = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
+    int result = binarysearch(arr, 0, n - 1, x);
+    cout << "Element found at index: " << result << endl;
 
     return 0;
 }
