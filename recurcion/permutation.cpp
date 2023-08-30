5e1424755179da12c59b7a0f8c775836e1f1659e
ruturaj2829 <ruturajsonkamble-ecs@atharvacoe.ac.in>
2023-08-30 19:50:45 +0530

    permute

---
+++ recurcion/permutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve(vector<int>& nums, int i, vector<vector<int>>& ans) {
+    // Base case
+    if (i >= nums.size()) {
+        ans.push_back(nums);
+        return;
+    }
+
+    for (int j = i; j < nums.size(); j++) {
+        swap(nums[i], nums[j]);
+        solve(nums, i + 1, ans);
+        swap(nums[i], nums[j]); // Backtrack by restoring the original state
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+    int i = 0;
+    solve(nums, i, ans);
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3,6,8,9};
+    vector<vector<int>> permutations = permute(nums);
+
+    for (const auto& perm : permutations) {
+        for (int num : perm) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
